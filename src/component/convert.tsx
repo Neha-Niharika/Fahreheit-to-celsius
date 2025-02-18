f8ac416702c7346e57871753ab46530ded6478a2
Neha-Niharika <64797011+Neha-Niharika@users.noreply.github.com>
2025-02-18 19:02:14 +0530

    fix-1

--- src/component/convert.tsx
+++ src/component/convert.tsx
@@ -13,11 +13,41 @@ function Convert() {
     setC(temp.toFixed(2));
   }
   return (
-    <div>
-      <label>Enter temperature in Fahrenheit</label>
-      <input value={tempF} onChange={viewF} />
-      <button onClick={handleChange}>Click Me</button>
-      <label>Temperature in Celsius {tempC}</label>
+    <div
+      className="container text-white"
+      style={{
+        backgroundColor: "#007bff",
+        padding: "20px",
+        borderRadius: "10px",
+        maxWidth: "500px",
+        margin: "30px auto",
+        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+      }}
+    >
+      <div className="row mb-3">
+        <div className="col">
+          <label className="form-label">Enter temperature in Fahrenheit</label>
+        </div>
+        <div className="col=12 cod-mod-6">
+          <input value={tempF} onChange={viewF} className="form-control" />
+        </div>
+      </div>
+      <div className="container text-center">
+        <div className="col justify-content-md-centerr">
+          <button
+            onClick={handleChange}
+            className="btn btn-danger"
+            style={{ color: "white" }}
+          >
+            Click Me
+          </button>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <label>Temperature in Celsius {tempC}°C</label>
+        </div>
+      </div>
     </div>
   );
 }
